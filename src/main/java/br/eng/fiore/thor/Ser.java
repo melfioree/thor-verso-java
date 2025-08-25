5993f96ba2cd1a85ed0afcd4c05be28b1352879a
unknown <melfiorecb@gmail.com>
2025-08-25 15:30:18 -0300

    classe ser

--- src/main/java/br/eng/fiore/thor/Ser.java
+++ src/main/java/br/eng/fiore/thor/Ser.java
@@ -1,10 +1,44 @@
+package br.eng.fiore.thor;
+
+import java.util.ArrayList;
 import java.util.List;
 
-
 public class Ser {
-private String nome;
-private boolean vivo;
-private String moradia;
-private List<Artefato> artefatos = new ArrayList <Artefato>();
-private List<Ser> filhos = new ArrayList <Ser>();
+    private String nome;
+    private boolean vivo;
+    private String moradia;
+    private List<Artefato> artefatos = new ArrayList<Artefato>();
+    private List<Ser> filhos = new ArrayList<Ser>();
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public boolean isVivo() {
+        return vivo;
+    }
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+    public String getMoradia() {
+        return moradia;
+    }
+    public void setMoradia(String moradia) {
+        this.moradia = moradia;
+    }
+    public List<Artefato> getArtefatos() {
+        return artefatos;
+    }
+    public void setArtefatos(List<Artefato> artefatos) {
+        this.artefatos = artefatos;
+    }
+    public List<Ser> getFilhos() {
+        return filhos;
+    }
+    public void setFilhos(List<Ser> filhos) {
+        this.filhos = filhos;
+    }
+
 }
